3a4b7500af97cca59c89e3b31031ffd694b529e6
Liliya-Tazieva <tazieva2107@gmail.com>
2017-12-12 01:07:14 +0300

    Changed service

---
+++ WebApplication1/Controllers/CalcController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.MathService;
+
+namespace WebApplication1.Controllers
+{
+    public class CalcController : Controller
+    {
+        public ActionResult Calculator()
+        {
+            return View();
+        }
+
+        public ActionResult Calculate(string FirstVariable, string SecondVariable, string action)
+        {
+            var client = new MathServiceClient();
+            string result = "";
+            if (!Double.TryParse(FirstVariable.Replace(',', '.'), out var firstVar))
+            {
+                result = "Incorrect first parameter";
+
+                ViewBag.Message = result;
+                return View();
+            }
+            if (!Double.TryParse(SecondVariable.Replace(',', '.'), out var secondVar))
+            {
+                result = "Incorrect second parameter";
+
+                ViewBag.Message = result;
+                return View();
+            }
+            switch (action)
+            {
+                case "Add":
+                    result = client.Add(firstVar, secondVar).ToString(CultureInfo.InvariantCulture);
+                    break;
+                case "Sub":
+                    result = client.Sub(firstVar, secondVar).ToString(CultureInfo.InvariantCulture);
+                    break;
+                case "Mult":
+                    result = client.Mul(firstVar, secondVar).ToString(CultureInfo.InvariantCulture);
+                    break;
+                default:
+                    result = SecondVariable != "0" ? client.Div(firstVar, secondVar).ToString(CultureInfo.InvariantCulture) 
+                        : "Incorrect parameters";
+                    break;
+            }
+
+            ViewBag.Message = result;
+            return View();
+        }
+    }
+}
