faa0eea8a637b8c324fed041565a1df75c8716ac
Liliya-Tazieva <tazieva2107@gmail.com>
2017-12-12 04:18:42 +0300

    Added unit tests

---
+++ UnitTestWebApplication1/ControllerTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using WebApplication1;
+using WebApplication1.Controllers;
+using WcfCalcApplication;
+
+namespace UnitTestWebApplication1
+{
+    [TestClass]
+    public class ControllerTests
+    {
+        [TestMethod]
+        public void TestMethodCalculate1()
+        {
+            //Arrange
+            var controller = new CalcController();
+            //Act
+            var result = controller.Calculate("22","43", "Add") as ViewResult;
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("65",result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void TestMethodCalculate2()
+        {
+            //Arrange
+            var controller = new CalcController();
+            //Act
+            var result = controller.Calculate("dd", "43", "Add") as ViewResult;
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Incorrect first parameter", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void TestMethodCalculate3()
+        {
+            //Arrange
+            var controller = new CalcController();
+            //Act
+            var result = controller.Calculate("22", "f", "Add") as ViewResult;
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Incorrect second parameter", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void TestMethodCalculate4()
+        {
+            //Arrange
+            var controller = new CalcController();
+            //Act
+            var result = controller.Calculate("22", "0", "Div") as ViewResult;
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Incorrect parameters", result.ViewBag.Message);
+        }
+        [TestMethod]
+        public void TestMethodCalculate5()
+        {
+            //Arrange
+            var controller = new CalcController();
+            //Act
+            var result = controller.Calculate("22,4", "43,3", "Add") as ViewResult;
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("65.7", result.ViewBag.Message);
+        }
+        [TestMethod]
+        public void TestMethodCalculate6()
+        {
+            //Arrange
+            var controller = new CalcController();
+            //Act
+            var result = controller.Calculate("22 5", "43", "Mul") as ViewResult;
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Incorrect first parameter", result.ViewBag.Message);
+        }
+    }
+}
---
+++ UnitTestWebApplication1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestWebApplication1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestWebApplication1")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cef5746b-8feb-4f59-bdf4-2a47b5a400cc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
